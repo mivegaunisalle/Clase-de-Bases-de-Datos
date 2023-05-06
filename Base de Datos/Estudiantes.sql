c49ba1767266d0a5566bbc956ddfdd4941c9592c
VEGA RUIZ MIGUEL ANGEL <mivega@unisalle.edu.co>
2023-05-06 13:04:47 -0500

    Se carga la base de datos de Estudiantes

--- Base de Datos/Estudiantes.sql
+++ Base de Datos/Estudiantes.sql
@@ -1,10 +1,54 @@
- Create Table Propietario 
-(
-	IdPropietario  int Not Null auto_increment,
-	Nombre Varchar(50) NOT null,
-	Apellido Varchar(50)  NOT null,
-	telefono numeric (10)  NOT null,
-	IdTipoDocumento int not null,
-	Primary Key(IdPropietario),
-	foreign key (IdTipoDocumento) references  TipoDocumento(IdTipoDocumento)
- );
+CREATE DATABASE hogwarts_db;
+
+USE hogwarts_db;
+
+CREATE TABLE estudiantes (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  casa VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE materias (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE asignaturas (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  id_estudiante INT(11) NOT NULL,
+  id_materia INT(11) NOT NULL,
+  calificacion INT(11),
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id),
+  FOREIGN KEY (id_materia) REFERENCES materias(id)
+);
+
+CREATE TABLE profesores (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE clases (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  id_materia INT(11) NOT NULL,
+  id_profesor INT(11) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_materia) REFERENCES materias(id),
+  FOREIGN KEY (id_profesor) REFERENCES profesores(id)
+);
+
+CREATE TABLE horarios (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  id_clase INT(11) NOT NULL,
+  dia_semana VARCHAR(20) NOT NULL,
+  hora_inicio TIME NOT NULL,
+  hora_fin TIME NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_clase) REFERENCES clases(id)
+);
